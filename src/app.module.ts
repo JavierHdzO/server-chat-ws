20b98c3716bbccc72d8823301584727062b5b427
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-01-25 22:08:42 -0600

    012523 F [CHORE]: add docker-compose file and config typeorm (postgres)

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,10 +15,19 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:process.env.HOST_DB,
+      port:Number(process.env.PORT_DB),
+      username:process.env.USERNAME_DB,
+      password:process.env.PASSWORD_DB,
+      database:process.env.NAME_DB,
+      entities:[],
+      synchronize: true
+    }),
     
     UsersModule
   ],
-  controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
--- src/config/validation.ts
+++ src/config/validation.ts
@@ -2,9 +2,9 @@ import * as joi from 'joi';
 
 export const validationSchema = joi.object({
     PORT: joi.number(),
-    HOST_DB: joi.string(),
-    PORT_DB: joi.string(),
-    USER_DB: joi.string(),
     PASSWORD_DB: joi.string(),
-    NAME_DB: joi.string()
+    USERNAME_DB: joi.string(),
+    NAME_DB: joi.string(),
+    HOST_DB: joi.string(),
+    PORT_DB: joi.string()
 });
